e80639d6a375fa1ae3ef7d98d62ff86e8f38a519
Mark Loeser <mark.loeser@gmail.com>
2017-01-17 06:46:39 -0500

    add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -4,15 +4,31 @@ var socket = io();
 
 console.log(name + ' wants to join ' + room);
 
+jQuery('.room-title').text(room);
+
+socket.on('joinRoom', function(req) {
+	socket.join(req.room);
+	socket.broadcast.to(req.room).emit('message', {
+		name: 'System',
+		text: req.name + ' has joined!',
+		timestamp: moment().valueOf()
+	})
+});
+
 socket.on('connect',function() {
 	console.log('Connected to socket.io server');
-})
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
+});
 
 socket.on('message', function(message){
 	var momentTimestamp = moment.utc(message.timestamp)
 	var $message = jQuery('.messages');
 	console.log('New Message:');
 	console.log(message.text);
+
 
 	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') +'</strong></p>');
 	$message.append('<p>' + message.text + '</p>');
